02493983178766df6556bdf1d92bdae66a8f7018
belu1238 <belencanalda5@gmail.com>
2024-01-10 21:14:38 -0300

    primer cambio

--- routes/libros.js
+++ routes/libros.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+
 const Libro = require("../models/Libro");
 
-router.get("/", async (req, res) => {
+const { requiredScopes } = require("express-oauth2-jwt-bearer");
+
+router.get("/", requiredScopes("read:productos"), async (req, res) => {
 try {
 const libros = await Libro.find();
 res.json(libros);
@@ -11,7 +14,7 @@ res.status(500).json({ error: "Error al obtener los libros" });
 }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", requiredScopes("write:productos"), async (req, res) => {
 try {
 const nuevoLibro = new Libro(req.body);
 await nuevoLibro.save();
@@ -21,7 +24,7 @@ res.status(500).json({ error: "Error al crear el Libro" });
 }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", requiredScopes("write:productos"), async (req, res) => {
 try {
 const Libro = await Libro.findByIdAndUpdate(req.params.id, req.body,
 {
@@ -33,7 +36,7 @@ res.status(500).json({ error: "Error al actualizar el Libro" });
 }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', requiredScopes("write:productos"), async (req, res) => {
 try {
 await Libro.findByIdAndDelete(req.params.id);
 res.json({ message: 'Libro eliminado correctamente' });
